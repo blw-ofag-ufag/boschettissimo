034e980504bc98545aa97e03b67dfed3b31ebb17
CecNyff <cecile.nyffeler@wsl.ch>
2026-04-08 16:12:52 +0200

    first draft of report for EB analysis (quarto document, html output, TBD)

---
+++ reporting/R001b_prepare_maps.R
@@ -0,0 +1,81 @@
+# Load libraries
+library(sf)
+library(leaflet)
+library(terra)
+
+# Preparation of the maps shown in R001_EB.qmd
+#---------------------------------------------
+tlm_eb_maps <- lapply(names(sa_vec), function(nm) {
+  
+  # Get study area bounding box
+  layer_bbox <- st_read(sa_vec[[nm]]$path, layer = "bb_box", quiet = TRUE) %>% 
+    st_transform(crs = "+proj=longlat +datum=WGS84") %>%
+    st_cast("MULTIPOLYGON") %>%
+    st_cast("POLYGON")
+  
+  # Get the tlm eb points
+  layer_tlm_eb <- st_read(sa_vec[[nm]]$path, layer = "tlm_bb_einzelbaum_gebuesch", quiet = TRUE) %>% 
+    st_transform(crs = "+proj=longlat +datum=WGS84") %>%
+    st_cast("MULTIPOINT") %>%
+    st_cast("POINT")
+  
+  # Get the vegetation height model
+  r_wgs <- project(sa_tif[[nm]], "EPSG:4326")
+  r_wgs <- aggregate(r_wgs, fun = "mean", fact = 4)
+  
+  # Create a palette for the VHM
+  pal <- colorNumeric("viridis", values(r_wgs), na.color = "transparent")
+  
+  # Compose the map
+  m <- leaflet() %>%
+    addPolygons(
+      data = layer_bbox,
+      fillColor = "cyan",
+      fillOpacity = 0,
+      color = "cyan",
+      weight = 2
+    ) %>%
+    addCircleMarkers(
+      data = layer_tlm_eb,
+      radius = 1,
+      color = "red",
+      fillOpacity = 0.5,
+      group = "tlm_bb_einzelbaum_gebuesch"
+    )  %>%
+    addRasterImage(
+      r_wgs,
+      colors = pal,
+      opacity = 0.8,
+      group = "vhm"
+    ) %>%
+    addWMSTiles(
+      "https://wmts10.geo.admin.ch/1.0.0/ch.swisstopo.swissimage-product/default/current/3857/{z}/{x}/{y}.jpeg",
+      layers = "swissimage-product",
+      group = "Aerial imagery - swisstopo",
+      layerId = "swissimage-product",
+      options = WMSTileOptions(format = "image/png", transparent = TRUE),
+      attribution = "swisstopo"
+    )  %>%
+    addWMSTiles(
+      "https://wmts10.geo.admin.ch/1.0.0/ch.swisstopo.pixelkarte-farbe/default/current/3857/{z}/{x}/{y}.jpeg",
+      layers = "pixelkarte-farbe",
+      group = "Location map color - swisstopo",
+      layerId = "pixelkarte-farbe",
+      options = WMSTileOptions(format = "image/png", transparent = TRUE),
+      attribution = "swisstopo"
+    ) %>%
+    addLayersControl(
+      baseGroups = c("Location map color - swisstopo", "Aerial imagery - swisstopo"),
+      overlayGroups = c("tlm_bb_einzelbaum_gebuesch","vhm"),
+      options = layersControlOptions(collapsed = TRUE)
+    ) %>%
+    addLegend(
+      pal = pal,
+      values = values(r_wgs),
+      title = "VHM"
+    )
+  
+  return(m)
+})
+
+names(tlm_eb_maps) <- names(sa_vec)
