75b39542f646cfa2652618cb06fbb81508a02700
CecNyff <cecile.nyffeler@wsl.ch>
2026-04-14 17:40:56 +0200

    tree seg first test across study areas using watershed algo

--- src/004_SegmentTrees.R
+++ src/004_SegmentTrees.R
@@ -19,25 +19,8 @@ sas <- read.csv2(file = paste0(git_root_path,"prj-data/study_areas.csv"), sep=",
 # Tree segementation
 #-----------------------------------------------------
 
-#*****************************************************
-#* lidR provides 4 segmentation algorithms
-#* watershed --> often splits one tree into multiparts
-#* li 2012 --> segmentation on point cloud. Tests inconclusive, paper mentions optimized for coniferous trees.
-#* dalponte 2016 --> region growing algorithm, paper mentions good for trees with dbh > 80cm, less for smaller trees.
-#* silva 2016 --> uses tree tops and vhm. From tests was the most conclusive one.
-#*****************************************************
-
-# Function that defines the moving window size based on height
-# TODO : first implementation based on test for sa rickenbach, 1st ALS file
-f <- function(h) {
-  ifelse(h > 4.5, 9, 3)
-}
-
 # Loop through the study areas
 for(sa in sas$id){
-  
-  # Initialize an empty tree top df
-  ttops_sa <- data.frame()
   
   # Filter the LAS files associated to the study area
   ALS_sa <- ALS_sas[which(ALS_sas$sa_id == sa),]
@@ -45,9 +28,11 @@ for(sa in sas$id){
   # Load the VHM associated to the study area
   vhm_sa <- rast(paste0(study_area_data_path,sa,"_vhm.tif"))
   
-  # Get two smoothed VHMS, one for the big trees identification, one for the crown delimitation
-  chm_7 <- focal(vhm_sa, w = matrix(1,7,7), fun = mean) # Big tree identification
-  chm_3 <- focal(vhm_sa, w = matrix(1,3,3), fun = mean) # Crown delimitation
+  # Have VHM in memory (for crown delimitation and also segmentation of big raster (engelswilen))
+  vhm_sa <- toMemory(vhm_sa)
+  
+  # Path to las files
+  las_dir <- paste0(study_area_data_path,sa,"_las/")
   
   # Load the las catalogue
   ctg <- readLAScatalog(las_dir)
@@ -60,48 +45,28 @@ for(sa in sas$id){
                  sas$upper_left_n[which(sas$id == sa)]+100)
   
   # Filter to keep only the vegetation points
-  las_veg <- filter_poi(las, Classification == 3)
+  las_veg <- filter_poi(las, Classification == 3); rm(las)
   
-  # Identify the big trees
-  ttops_big <- locate_trees(chm_7, lmf(ws= f, hmin = 1.37, shape="circular"))
-  ttops_big$treeSize <- "big"
+  # Segment the trees using the watershed algorithm
+  las_eb <- segment_trees(
+    las_veg,
+    lidR::watershed(vhm_sa, th_tree = 2, ext = 2)
+  ); rm(las_veg)
+  # plot(las_eb, bg = "white", size = 4, color = "treeID")
   
-  # Identify the small new trees
-  # TODO see if this should be kept or not, output not super ideal, test with several study areas
-  ttops_small <- locate_trees(vhm_sa, lmf(ws= f, hmin = 1.37, shape="circular"))
+  # Export the segmented trees
+  writeLAS(las_eb, paste0(study_area_data_path,sa,"_segmented_trees.laz")); rm(las_eb)
   
-  # Find nearest big trees point for each small tree point and compute the distance between them
-  nearest_big <- st_nearest_feature(ttops_small, ttops_big)
-  dist_nearest <- st_distance(
-    ttops_small,
-    ttops_big[nearest_big, ],
-    by_element = TRUE
-  )
-  
-  # Filter to keep only small trees that are at least 10m away from an already identified big tree
-  ttops_small_filtered <- ttops_small[dist_nearest > units::set_units(7, "m"), ]
-  ttops_small_filtered$treeSize <- "small"
-  
-  # Bind them together and to the study area treetop df
-  ttops_sa <- rbind(ttops_sa,ttops_big, ttops_small_filtered)
-  
-  # Recreate the tree id column
-  ttops_sa$treeID <- seq_len(nrow(ttops_sa))
-  
-  # Segment the trees from the identified tree tops
-  eb_sa <- segment_trees(las_veg, silva2016(chm_3, ttops_sa, max_cr_factor=1))
-  # plot(eb_sa, bg = "white", size = 4, color = "treeID")
-
   # Keep only single trees that are on wanted LWN areas
   # TODO
   
-  # Export the tree tops 
-  st_write(ttops_sa, dsn=paste0(study_area_data_path,sa,".gpkg"), layer="treetops", append = F)
-  
-  # and the crowns to compare to other products
-  # TODO check if geom="concave", concaveman = c(5,0.5) or geom="convex" or any other combination would be best
-  crowns <- crown_metrics(eb_sa, func = NULL, treeID = "IDws", geom="convex")
-  st_write(crowns, dsn=paste0(study_area_data_path,sa,".gpkg"), layer="crowns", append = F)
+  # Get the 2D crown delimitation of the single trees using the same algorithm
+  seg_algo <- lidR::watershed(vhm_sa, th_tree = 2, ext = 2)
+  crowns <- seg_algo()
+  crown_polys <- as.polygons(crowns, dissolve = TRUE); rm(vhm_sa); rm(crowns);
+
+  # Export the crowns to compare to other products
+  writeVector(crown_polys, filename=paste0(study_area_data_path,sa,".gpkg"), layer="crowns", insert = TRUE, overwrite = TRUE)
   
 }
 
