91ddcb55aaa6a64194784e2c387fdd2385273af7
CecNyff <cecile.nyffeler@wsl.ch>
2026-04-28 17:33:43 +0200

    updating paths for copying html

--- reporting/render_reports.R
+++ reporting/render_reports.R
@@ -7,12 +7,12 @@ quarto::quarto_render("reporting/R001_EB.qmd")
 
 # Move the report to speedy
 file.copy(
-  "R001_EB.html",
+  "reporting/R001_EB.html",
   "//speedy16-36/data_15/_PROJEKTE/20260401_Boschettissimo/02_Reports/R001_EB.html",
   overwrite = TRUE
 )
 file.copy(
-  "R001_EB_files",
+  "reporting/R001_EB_files",
   "//speedy16-36/data_15/_PROJEKTE/20260401_Boschettissimo/02_Reports/",
   overwrite = TRUE,
   recursive = TRUE
