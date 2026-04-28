83c1a7c5a80ae79177d7aad500b804a05fbb6f1e
CecNyff <cecile.nyffeler@wsl.ch>
2026-04-28 12:43:45 +0200

    commenting out las segmentation, no need for segmented tree points at the moment

--- src/004_SegmentTrees.R
+++ src/004_SegmentTrees.R
@@ -31,31 +31,34 @@ for(sa in sas$id){
   # Have VHM in memory (for crown delimitation and also segmentation of big raster (engelswilen))
   vhm_sa <- toMemory(vhm_sa)
   
-  # Path to las files
-  las_dir <- paste0(study_area_data_path,sa,"_las/")
-  
-  # Load the las catalogue
-  ctg <- readLAScatalog(las_dir)
-  
-  # Clip to the study area
-  las <- clip_rectangle(ctg, 
-                 sas$upper_left_e[which(sas$id == sa)]-100,
-                 sas$lower_right_n[which(sas$id == sa)]-100,
-                 sas$lower_right_e[which(sas$id == sa)]+100,
-                 sas$upper_left_n[which(sas$id == sa)]+100)
-  
-  # Filter to keep only the vegetation points
-  las_veg <- filter_poi(las, Classification == 3); rm(las)
-  
-  # Segment the trees using the watershed algorithm
-  las_eb <- segment_trees(
-    las_veg,
-    lidR::watershed(vhm_sa, th_tree = 2, ext = 2)
-  ); rm(las_veg)
-  # plot(las_eb, bg = "white", size = 4, color = "treeID")
-  
-  # Export the segmented trees
-  writeLAS(las_eb, paste0(study_area_data_path,sa,"_segmented_trees.laz")); rm(las_eb)
+  # #***************************************************************************
+  # # COMMENTED OUT, SEGMENTED LAS NOT NEEDED AT THE MOMENT, CHECK IN FUTURE
+  # # Path to las files
+  # las_dir <- paste0(study_area_data_path,sa,"_las/")
+  # 
+  # # Load the las catalogue
+  # ctg <- readLAScatalog(las_dir)
+  # 
+  # # Clip to the study area
+  # las <- clip_rectangle(ctg, 
+  #                sas$upper_left_e[which(sas$id == sa)]-100,
+  #                sas$lower_right_n[which(sas$id == sa)]-100,
+  #                sas$lower_right_e[which(sas$id == sa)]+100,
+  #                sas$upper_left_n[which(sas$id == sa)]+100)
+  # 
+  # # Filter to keep only the vegetation points
+  # las_veg <- filter_poi(las, Classification == 3); rm(las)
+  # 
+  # # Segment the trees using the watershed algorithm
+  # las_eb <- segment_trees(
+  #   las_veg,
+  #   lidR::watershed(vhm_sa, th_tree = 2, ext = 2)
+  # ); rm(las_veg)
+  # # plot(las_eb, bg = "white", size = 4, color = "treeID")
+  # 
+  # # Export the segmented trees
+  # writeLAS(las_eb, paste0(study_area_data_path,sa,"_segmented_trees.laz")); rm(las_eb)
+  # #***************************************************************************
   
   # Keep only single trees that are on wanted LWN areas
   # TODO
