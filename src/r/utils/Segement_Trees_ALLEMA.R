e5d149fadec6e1582191dbf72c024e80ff71c93e
CecNyff <cecile.nyffeler@wsl.ch>
2026-06-05 09:56:58 +0200

    correct rast() step that erased seg

--- src/r/utils/Segement_Trees_ALLEMA.R
+++ src/r/utils/Segement_Trees_ALLEMA.R
@@ -44,7 +44,7 @@ prep_vhm <- function(vhm_path, e_buf){
   
 }
 
-seg_cell <- function(vhm_cell, e_buf, LN_2025_path, outname, ALLEMA_ID){
+seg_cell <- function(vhm_cell, e_buf, LN_2025_path, outname, ALLEMA_filter){
   
   
   # Set the filter (area to consider) step
@@ -66,13 +66,6 @@ seg_cell <- function(vhm_cell, e_buf, LN_2025_path, outname, ALLEMA_ID){
   # # TEMP - Just union all the geoms, later keep only relevant ones
   # ln_filter <- st_union(ln_sa) %>%
   #   st_transform(crs(vhm_cell))
-  
-  # Get the forest mask used in ALLEMA to be able to compare the same single trees (and actually also Feldgehoelze etc)
-  ALLEMA_filter <- st_read("//speedy16-36/data_15/_PROJEKTE/20260401_Boschettissimo/01_Daten/GIS/ORIG_DATA/ALLEMA/Wald_Gehoelz_ErhZyk2.gdb", 
-                         query = paste0("SELECT * FROM Wald_Gehoelz_ErhZyk2_3D WHERE FK_Quadrat = ", ALLEMA_ID, " AND Gehoelztyp IN (50,58)"))
-  
-  ALLEMA_filter <- st_union(ALLEMA_filter) %>%
-    st_transform(crs(vhm_cell))
     
   # Segmentation step
   #-----------------------------------------------------
@@ -91,69 +84,68 @@ seg_cell <- function(vhm_cell, e_buf, LN_2025_path, outname, ALLEMA_ID){
     # Remove unused objects
     #-----------------------------------------------------
 
-    
     vhm_sa_smooth3_mean <- focal(vhm_cell, w = matrix(1,3,3), fun = mean)
-    crowns_ws_mw3_mean <- lidR::watershed(vhm_sa_smooth3_mean)() %>% rast() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
+    crowns_ws_mw3_mean <- lidR::watershed(vhm_sa_smooth3_mean)() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
     crowns_ws_mw3_mean <- crowns_ws_mw3_mean[!st_intersects(crowns_ws_mw3_mean, ALLEMA_filter, sparse = FALSE), ]
     write_sf(crowns_ws_mw3_mean, dsn=outname, layer=paste0("vhm",res_cm,"cm_crowns_ws_mw3_mean"), delete_dsn = TRUE)
     rm(vhm_sa_smooth3_mean); rm(crowns_ws_mw3_mean)
     
     vhm_sa_smooth5_mean <- focal(vhm_cell, w = matrix(1,5,5), fun = mean)
-    crowns_ws_mw5_mean <- lidR::watershed(vhm_sa_smooth5_mean)() %>% rast() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
+    crowns_ws_mw5_mean <- lidR::watershed(vhm_sa_smooth5_mean)() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
     crowns_ws_mw5_mean <- crowns_ws_mw5_mean[!st_intersects(crowns_ws_mw5_mean, ALLEMA_filter, sparse = FALSE), ]
     write_sf(crowns_ws_mw5_mean, dsn=outname, layer=paste0("vhm",res_cm,"cm_crowns_ws_mw5_mean"), append = TRUE)
     rm(vhm_sa_smooth5_mean); rm(crowns_ws_mw5_mean)
     
     vhm_sa_smooth3_max <- focal(vhm_cell, w = matrix(1,3,3), fun = max)
-    crowns_ws_mw3_max <- lidR::watershed(vhm_sa_smooth3_max)() %>% rast() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
+    crowns_ws_mw3_max <- lidR::watershed(vhm_sa_smooth3_max)() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
     crowns_ws_mw3_max <- crowns_ws_mw3_max[!st_intersects(crowns_ws_mw3_max, ALLEMA_filter, sparse = FALSE), ]
     write_sf(crowns_ws_mw3_max, dsn=outname, layer=paste0("vhm",res_cm,"cm_crowns_ws_mw3_max"), append = TRUE)
     rm(vhm_sa_smooth3_max); rm(crowns_ws_mw3_max)
     
     vhm_sa_smooth5_max <- focal(vhm_cell, w = matrix(1,5,5), fun = max)
-    crowns_ws_mw5_max <- lidR::watershed(vhm_sa_smooth5_max)() %>% rast() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
+    crowns_ws_mw5_max <- lidR::watershed(vhm_sa_smooth5_max)() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
     crowns_ws_mw5_max <- crowns_ws_mw5_max[!st_intersects(crowns_ws_mw5_max, ALLEMA_filter, sparse = FALSE), ]
     write_sf(crowns_ws_mw5_max, dsn=outname, layer=paste0("vhm",res_cm,"cm_crowns_ws_mw5_max"), append = TRUE)
     rm(vhm_sa_smooth5_max); rm(crowns_ws_mw5_max)
     
     g03 <- focalMat(vhm_cell, d = 0.3, type = "Gauss")
     vhm_sa_gauss03 <- focal(vhm_cell, w = g03, fun = sum)
-    crowns_gauss03 <- lidR::watershed(vhm_sa_gauss03)() %>% rast() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
+    crowns_gauss03 <- lidR::watershed(vhm_sa_gauss03)() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
     crowns_gauss03 <- crowns_gauss03[!st_intersects(crowns_gauss03, ALLEMA_filter, sparse = FALSE), ]
     write_sf(crowns_gauss03, dsn=outname, layer=paste0("vhm",res_cm,"cm_crowns_gauss03"), append = TRUE)
     rm(vhm_sa_gauss03); rm(crowns_gauss03); rm(g03)
     
     g05 <- focalMat(vhm_cell, d = 0.5, type = "Gauss")
     vhm_sa_gauss05 <- focal(vhm_cell, w = g05, fun = sum)
-    crowns_gauss05 <- lidR::watershed(vhm_sa_gauss05)() %>% rast() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
+    crowns_gauss05 <- lidR::watershed(vhm_sa_gauss05)() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
     crowns_gauss05 <- crowns_gauss05[!st_intersects(crowns_gauss05, ALLEMA_filter, sparse = FALSE), ]
     write_sf(crowns_gauss05, dsn=outname, layer=paste0("vhm",res_cm,"cm_crowns_gauss05"), append = TRUE)
     rm(vhm_sa_gauss05); rm(crowns_gauss05); rm(g05)
     
     g07 <- focalMat(vhm_cell, d = 0.7, type = "Gauss")
     vhm_sa_gauss07 <- focal(vhm_cell, w = g07, fun = sum)
-    crowns_gauss07 <- lidR::watershed(vhm_sa_gauss07)() %>% rast() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
+    crowns_gauss07 <- lidR::watershed(vhm_sa_gauss07)() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
     crowns_gauss07 <- crowns_gauss07[!st_intersects(crowns_gauss07, ALLEMA_filter, sparse = FALSE), ]
     write_sf(crowns_gauss07, dsn=outname, layer=paste0("vhm",res_cm,"cm_crowns_gauss07"), append = TRUE)
     rm(vhm_sa_gauss07); rm(crowns_gauss07); rm(g07)
     
     g1 <- focalMat(vhm_cell, d = 1, type = "Gauss")
     vhm_sa_gauss1 <- focal(vhm_cell, w = g1, fun = sum)
-    crowns_gauss1 <- lidR::watershed(vhm_sa_gauss1)() %>% rast() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
+    crowns_gauss1 <- lidR::watershed(vhm_sa_gauss1)() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
     crowns_gauss1 <- crowns_gauss1[!st_intersects(crowns_gauss1, ALLEMA_filter, sparse = FALSE), ]
     write_sf(crowns_gauss1, dsn=outname, layer=paste0("vhm",res_cm,"cm_crowns_gauss1"), append = TRUE)
     rm(vhm_sa_gauss1); rm(crowns_gauss1); rm(g1)
     
     g2 <- focalMat(vhm_cell, d = 2, type = "Gauss")
     vhm_sa_gauss2 <- focal(vhm_cell, w = g2, fun = sum)
-    crowns_gauss2 <- lidR::watershed(vhm_sa_gauss2)() %>% rast() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
+    crowns_gauss2 <- lidR::watershed(vhm_sa_gauss2)() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
     crowns_gauss2 <- crowns_gauss2[!st_intersects(crowns_gauss2, ALLEMA_filter, sparse = FALSE), ]
     write_sf(crowns_gauss2, dsn=outname, layer=paste0("vhm",res_cm,"cm_crowns_gauss2"), append = TRUE)
     rm(vhm_sa_gauss2); rm(crowns_gauss2); rm(g2)
     
     g3 <- focalMat(vhm_cell, d = 3, type = "Gauss")
     vhm_sa_gauss3 <- focal(vhm_cell, w = g3, fun = sum)
-    crowns_gauss3 <- lidR::watershed(vhm_sa_gauss3)() %>% rast() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
+    crowns_gauss3 <- lidR::watershed(vhm_sa_gauss3)() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
     crowns_gauss3 <- crowns_gauss3[!st_intersects(crowns_gauss3, ALLEMA_filter, sparse = FALSE), ]
     write_sf(crowns_gauss3, dsn=outname, layer=paste0("vhm",res_cm,"cm_crowns_gauss3"), append = TRUE)
     rm(vhm_sa_gauss3); rm(crowns_gauss3); rm(g3)
@@ -161,40 +153,40 @@ seg_cell <- function(vhm_cell, e_buf, LN_2025_path, outname, ALLEMA_ID){
   } else { # VHM with 1 m resolution
     
 
-    crowns_ws_raw <- lidR::watershed(vhm_cell)() %>% rast() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
+    crowns_ws_raw <- lidR::watershed(vhm_cell)() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
     crowns_ws_raw <- crowns_ws_raw[!st_intersects(crowns_ws_raw, ALLEMA_filter, sparse = FALSE), ]
     write_sf(crowns_ws_raw, dsn=outname, layer=paste0("vhm",res_cm,"cm_crowns_ws_raw"), append = TRUE)
     rm(crowns_ws_raw)
     
     vhm_sa_smooth3_mean <- focal(vhm_cell, w = matrix(1,3,3), fun = mean)
-    crowns_ws_mw3_mean <- lidR::watershed(vhm_sa_smooth3_mean)() %>% rast() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
+    crowns_ws_mw3_mean <- lidR::watershed(vhm_sa_smooth3_mean)() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
     crowns_ws_mw3_mean <- crowns_ws_mw3_mean[!st_intersects(crowns_ws_mw3_mean, ALLEMA_filter, sparse = FALSE), ]
     write_sf(crowns_ws_mw3_mean, dsn=outname, layer=paste0("vhm",res_cm,"cm_crowns_ws_mw3_mean"), append = TRUE)
     rm(vhm_sa_smooth3_mean); rm(crowns_ws_mw3_mean)
     
     vhm_sa_smooth3_max <- focal(vhm_cell, w = matrix(1,3,3), fun = max)
-    crowns_ws_mw3_max <- lidR::watershed(vhm_sa_smooth3_max)() %>% rast() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
+    crowns_ws_mw3_max <- lidR::watershed(vhm_sa_smooth3_max)() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
     crowns_ws_mw3_max <- crowns_ws_mw3_max[!st_intersects(crowns_ws_mw3_max, ALLEMA_filter, sparse = FALSE), ]
     write_sf(crowns_ws_mw3_max, dsn=outname, layer=paste0("vhm",res_cm,"cm_crowns_ws_mw3_max"), append = TRUE)
     rm(vhm_sa_smooth3_max); rm(crowns_ws_mw3_max)
     
     g05 <- focalMat(vhm_cell, d = 0.5, type = "Gauss")
     vhm_sa_gauss05 <- focal(vhm_cell, w = g05, fun = sum)
-    crowns_gauss05 <- lidR::watershed(vhm_sa_gauss05)() %>% rast() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
+    crowns_gauss05 <- lidR::watershed(vhm_sa_gauss05)() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
     crowns_gauss05 <- crowns_gauss05[!st_intersects(crowns_gauss05, ALLEMA_filter, sparse = FALSE), ]
     write_sf(crowns_gauss05, dsn=outname, layer=paste0("vhm",res_cm,"cm_crowns_gauss05"), append = TRUE)
     rm(vhm_sa_gauss05); rm(crowns_gauss05); rm(g05)
     
     g07 <- focalMat(vhm_cell, d = 0.7, type = "Gauss")
     vhm_sa_gauss07 <- focal(vhm_cell, w = g07, fun = sum)
-    crowns_gauss07 <- lidR::watershed(vhm_sa_gauss07)() %>% rast() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
+    crowns_gauss07 <- lidR::watershed(vhm_sa_gauss07)() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
     crowns_gauss07 <- crowns_gauss07[!st_intersects(crowns_gauss07, ALLEMA_filter, sparse = FALSE), ]
     write_sf(crowns_gauss07, dsn=outname, layer=paste0("vhm",res_cm,"cm_crowns_gauss07"), append = TRUE)
     rm(vhm_sa_gauss07); rm(crowns_gauss07); rm(g07)
     
     g1 <- focalMat(vhm_cell, d = 1, type = "Gauss")
     vhm_sa_gauss1 <- focal(vhm_cell, w = g1, fun = sum)
-    crowns_gauss1 <- lidR::watershed(vhm_sa_gauss1)() %>% rast() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
+    crowns_gauss1 <- lidR::watershed(vhm_sa_gauss1)() %>% as.polygons(dissolve = TRUE) %>% st_as_sf() %>% st_transform(crs(vhm_cell))
     crowns_gauss1 <- crowns_gauss1[!st_intersects(crowns_gauss1, ALLEMA_filter, sparse = FALSE), ]
     write_sf(crowns_gauss1, dsn=outname, layer=paste0("vhm",res_cm,"cm_crowns_gauss1"), append = TRUE)
     rm(vhm_sa_gauss1); rm(crowns_gauss1); rm(g1)
@@ -286,8 +278,19 @@ process_cell <- function(i) {
   # Crop the VHM to needed extent, and load it to memory so that can be used by lidR
   vhm_cell <- prep_vhm(VHM_S1_path, e_buf) 
   
+  # Filter on which surfaces segmentation should happen
+  #-----------------------------------------------------
+  
+  # Get the forest mask used in ALLEMA to be able to compare the same single trees (and actually also Feldgehoelze etc)
+  ALLEMA_filter <- st_read("//speedy16-36/data_15/_PROJEKTE/20260401_Boschettissimo/01_Daten/GIS/ORIG_DATA/ALLEMA/Wald_Gehoelz_ErhZyk2.gdb", 
+                           query = paste0("SELECT * FROM Wald_Gehoelz_ErhZyk2_3D WHERE FK_Quadrat = ", ALLEMA_Q[i, ]$ID_Quadrat, " AND Gehoelztyp IN (50,58)"))
+  
+  ALLEMA_filter <- st_union(ALLEMA_filter) %>%
+    st_transform(crs(vhm_cell)) %>%
+    st_buffer(5)
+  
   # Perform the segmentation
-  seg_cell(vhm_cell, e_buf, LN_2025_path, local_out, ALLEMA_Q[i, ]$ID_Quadrat)
+  seg_cell(vhm_cell, e_buf, LN_2025_path, local_out, ALLEMA_filter)
   
   #-----------------------------------------------------
   # VHM 1 m
@@ -297,7 +300,7 @@ process_cell <- function(i) {
   vhm_cell <- prep_vhm(VHM_S1_1m_path, e_buf)
   
   # Perform the segmentation
-  seg_cell(vhm_cell, e_buf, LN_2025_path, local_out, ALLEMA_Q[i, ]$ID_Quadrat)
+  seg_cell(vhm_cell, e_buf, LN_2025_path, local_out, ALLEMA_filter)
 
   
   # Move the files to the final location
