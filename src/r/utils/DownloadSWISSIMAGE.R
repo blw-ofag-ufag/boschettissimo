3b15a1af0aeb40633746bad57906e830260257c9
CecNyff <cecile.nyffeler@wsl.ch>
2026-07-28 15:00:48 +0200

    identifying SWISSIMAGE urls for which we do not have any LN intersecting parcel in 2025

--- src/r/utils/DownloadSWISSIMAGE.R
+++ src/r/utils/DownloadSWISSIMAGE.R
@@ -4,6 +4,7 @@
 
 # Libraries
 library(future.apply)
+library(sf)
 
 # Sourcing initialization code (paths and such)
 source("src/r/001_Initialization.R")
@@ -43,3 +44,32 @@ future_lapply(
   },
   future.seed = TRUE
 )
+
+#-----------------------------------------------------
+# Flag tiles that intersect the LN parcels
+#-----------------------------------------------------
+
+# Build each tile's bounding box directly from its tile ID in the URL
+tile_id <- regmatches(urls, regexpr("[0-9]{4}-[0-9]{4}", urls))
+tile_xy <- do.call(rbind, strsplit(tile_id, "-"))
+xmin <- as.numeric(tile_xy[, 1]) * 1000
+ymin <- as.numeric(tile_xy[, 2]) * 1000
+
+tile_wkt <- sprintf(
+  "POLYGON((%1$f %2$f, %3$f %2$f, %3$f %4$f, %1$f %4$f, %1$f %2$f))",
+  xmin, ymin, xmin + 1000, ymin + 1000
+)
+tiles <- st_sf(url = urls, geometry = st_as_sfc(tile_wkt, crs = 2056))
+
+# Only load the LN parcels falling within the extent covered by the tiles
+aoi_wkt <- st_as_text(st_as_sfc(st_bbox(tiles)))
+LN <- st_read(LN_2025_path, wkt_filter = aoi_wkt)
+
+# Flag tiles that intersect at least one LN parcel
+tiles$LN_mask <- lengths(st_intersects(tiles, LN)) > 0
+
+# Check out which lines do not have an intersecting LN parcel
+tiles[which(tiles$LN_mask == FALSE),] # 148 out of 1155
+
+# Save the updated tile list, keeping the url and LN_mask columns
+write.csv(st_drop_geometry(tiles), "data/SWISSIMAGE_2025_TG.csv", row.names = FALSE)
