9478123f9d98dde807944112ccd2e9d6df07b436
CecNyff <cecile.nyffeler@wsl.ch>
2026-07-28 14:10:33 +0200

    updating initialization file to remove obsolete paths

--- src/r/001_Initialization.R
+++ src/r/001_Initialization.R
@@ -4,26 +4,17 @@
 
 # General paths
 orig_data_path <- "//speedy16-36/data_15/_PROJEKTE/20260401_Boschettissimo/01_Daten/GIS/ORIG_DATA/"
-study_area_data_path <- "//speedy16-36/data_15/_PROJEKTE/20260401_Boschettissimo/01_Daten/GIS/STUDY_AREA_DATA/"
 
 # Data specific paths
 VHM_S1_path <- "//speedy12-37/data_17/_GEOBASISDATEN/_ENVIDAT/VHM_LiDAR_NFI/2024/landesforstinventar-vegetationshoehenmodell_lidar_2024_2056.tif"
 VHM_S1_1m_path <- "//speedy11-12-fs/data_17/_LIDAR/ALS_CH/__VHM_ALS_CH/20250217_VHM_ALS_CH_SWISS_1m_2056.tif"
 VHM_S2_path <- "//speedy11-12-fs/data_17/_LIDAR/ALS_CH/__VHM_ALS_CH/_Kantone/SWISS2_forBoschettissimo/2024_05_VHM_ALS_CH_SWISS2_0.5m_2056.tif"
 LN_2025_path <- paste0(orig_data_path,"BLW/LWB_Nutzungsflaechen_Derivat_BGDI_2025.gdb")
-ALS_path <- "//speedy11-12-fs/data_17/_LIDAR/ALS_CH/nDSM/"
 CH_1000_path <- "//speedy16-36/data_15/_PROJEKTE/_SNAPRASTER/ExampleRaster/ExampleRaster_CHbin_1000m_LV95.tif"
-ALS_ndms <- "//speedy11-12-fs/data_17/_LIDAR/ALS_CH/ALS_CH_db/mv_als_ch_ndsm_20260511.gpkg"
 forest_mask_path <- "//speedy11-12-fs/data_17/_GEOBASISDATEN/DATA_2025/ID164.23_Waldmaske/forest_mask_switzerland_shrub_20250902_v1/NFI_forest_mask_20250217.gpkg"
 settlement_path <- "//speedy16-36/data_15/_PROJEKTE/20260401_Boschettissimo/01_Daten/GIS/ORIG_DATA/SIEDLUNG/siedlung_2025_2056.gpkg"
 swissimage_path <- paste0(orig_data_path,"SWISSIMAGE/")
 
 # Local paths (for faster processing)
-orig_data_local_path <- "D:/BOSCHETTISSIMO/ORIG_DATA/"
-VHM_S2_tif_local_path <- "D:/BOSCHETTISSIMO/ORIG_DATA/VHM/"
-VHM_S2_local_path <- "D:/BOSCHETTISSIMO/ORIG_DATA/VHM/VHM_SWISS2.vrt"
 treeseg_data_local_path <- "D:/BOSCHETTISSIMO/PROCESSED_DATA/TREE_SEG/"
-
-# Processing specific paths (personal, to be changed)
-osgeo4w_path <- "C:/Program Files/QGIS 3.40.2/OSGeo4W.bat"
 
